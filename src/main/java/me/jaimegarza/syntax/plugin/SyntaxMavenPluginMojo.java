f4464272cd60fa4ed4b783edd0ab852c13728996
Jaime Garza <jaimegarza@gmail.com>
2016-03-08 17:15:04 -0800

    1.0.4 uses syntax 4.1.0 (in progress)

--- src/main/java/me/jaimegarza/syntax/plugin/SyntaxMavenPluginMojo.java
+++ src/main/java/me/jaimegarza/syntax/plugin/SyntaxMavenPluginMojo.java
@@ -30,9 +30,9 @@ package me.jaimegarza.syntax.plugin;
 
 import java.io.File;
 
-import me.jaimegarza.syntax.AnalysisException;
-import me.jaimegarza.syntax.OutputException;
-import me.jaimegarza.syntax.ParsingException;
+import me.jaimegarza.syntax.exception.AnalysisException;
+import me.jaimegarza.syntax.exception.OutputException;
+import me.jaimegarza.syntax.exception.ParsingException;
 import me.jaimegarza.syntax.Syntax;
 import me.jaimegarza.syntax.env.Environment;
 import me.jaimegarza.syntax.language.Language;
